887894eb9332835b466911b356466c2ba52d0079
unknown <baraklevy20@gmail.com>
2020-09-27 19:31:15 +0300

    Add basic functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const textToImage = require('text-to-image');
+
+const app = express();
+const port = 3000;
+let counter = 0;
+
+app.get('/counter.png', async (req, res) => {
+  counter += 1;
+  const dataUri = await textToImage.generate(
+    `My profile was viewed ${counter} times`,
+  );
+  const img = Buffer.from(dataUri.split(',')[1], 'base64');
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': img.length,
+  });
+
+  res.end(img);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
