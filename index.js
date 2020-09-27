b2cb59b661a5f27656c727b2e23f0cf38dcea0af
unknown <baraklevy20@gmail.com>
2020-09-27 20:24:58 +0300

    Change counter url

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const textToImage = require('text-to-image');
 const Mustache = require('mustache');
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 let counter = 0;
 
-app.get('/counter.png', async (req, res) => {
+app.get('/view-count.svg', async (req, res) => {
   counter += 1;
   const template = fs.readFileSync('./view-count.svg').toString();
   const viewSvg = Mustache.render(template, {counter});
